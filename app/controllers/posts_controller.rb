62122d0f6755b7372d548164caad83ab4b7bb86a
Kazuya-Sakashita <kz0508@gmail.com>
2020-08-21 06:35:52 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,65 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: 'desc')
+  end
+ 
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    
+  end
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.del_flag = 0
+    
+    if @post.save
+      redirect_to root_path
+    else
+      # flash[:alert] = 'Save error!'
+      #   render:index
+      # render plain: @post.errors.inspect
+      render 'new'
+    end
+  end
+
+    def update
+      @post = Post.find_by(id: params[:id])
+      if @post.update_attributes(post_params)
+        redirect_to root_path
+      else
+        render:edit
+        
+      end
+
+    end
+
+    def delete
+      @posts = Post.all.order(created_at: 'desc')
+    end
+
+    def destroy
+      @post = Post.find_by(id: params[:id])
+      # if @post.destroy
+      if @post.del_flag == 0
+         @post.del_flag = 1
+         @post.save
+         redirect_to root_path
+      else
+         render :index
+      end
+    end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+
+  root 'posts#index'
+ 
+  get 'posts',to:'posts#index'
+
+  get '/new' ,to: 'posts#new' ,as: 'new'
+
+  # get '/edit' ,to: 'posts#edit' ,as: 'edit'
+
+  post '/create', to: 'posts#create', as: 'create'
+
+  # get 'posts/:id/edit', to: 'posts#edit', as: 'edit'
+ 
+  patch 'posts/:id/update', to: 'posts#update', as:'update'
+
+  get 'posts/:id/show' ,to: 'posts#show' ,as:'show'
+  get 'posts/:id/edit', to: 'posts#edit', as: 'edit'
+
+  get 'posts/:id/destroy' ,to: 'posts#destroy' ,as:'destroy'
+
+  get 'posts/delete', to: 'posts#delete',as: 'delete'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
