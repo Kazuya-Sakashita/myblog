263ee5da9f7c39055b1ec51c579f8a3e0a5a0329
Kazuya-Sakashita <kz0508@gmail.com>
2020-10-18 19:45:32 +0900

    routes修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,19 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find_by(id: params[:id])
     @post.comments.create(comment_params)
-    redirect_to root_path
+    redirect_to show_path(@post)
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @comment = @post.comments.find(params[:comment_id])
+    @comment.destroy
+    redirect_to show_path(@post)
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body, :post_id)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts do
-    resources :comments
-  end
-  
   root 'posts#index'
- 
   get 'posts',to:'posts#index'
 
   get '/new' ,to: 'posts#new' ,as: 'new'
@@ -14,7 +9,7 @@ Rails.application.routes.draw do
   post '/create', to: 'posts#create', as: 'create'
 
   # get 'posts/:id/edit', to: 'posts#edit', as: 'edit'
- 
+
   patch 'posts/:id/update', to: 'posts#update', as:'update'
 
   get 'posts/:id/show' ,to: 'posts#show' ,as:'show'
@@ -23,6 +18,11 @@ Rails.application.routes.draw do
   get 'posts/:id/destroy' ,to: 'posts#destroy' ,as:'destroy'
 
   get 'posts/delete', to: 'posts#delete',as: 'delete'
+
+          #commentについてのルート設定
+  get  '/posts/:id/comments', to:'comments#index',as: 'post_comments'
+  post '/posts/:id/comments', to:'comments#create'
+  get '/posts/:id/comments/:comment_id', to:'comments#destroy',as: 'post_comments_delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
